9864e6e96e3cc504f9f458ff940e358d46226b5d
FR13NDS <fr13nds.2022@gmail.com>
2023-10-05 17:35:58 +0300

    rectangle

---
+++ learning-lessons/rectangles/src/main.rs
@@ -0,0 +1,26 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {// Everything within this impl block will be associated with the Rectangle type.
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+}
+
+
+// Everything within this impl block will be associated with the Rectangle type.
